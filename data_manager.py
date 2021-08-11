ea0e503fe0f97bfba2f06cec2682775c75d81268
Matt Wilke <matt.wilke08@gmail.com>
2021-08-10 22:07:45 -0500

    update to add testing to sheet

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,19 @@
+import requests
+from pprint import pprint
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self, url):
+        self.airport_sheet = url
+
+    def get_sheet(self):
+        response = requests.get(url=self.airport_sheet)
+        data = response.json()
+        return data['prices']
+
+    def set_iata_code(self, city, code):
+        body = {
+            "city": city,
+            "iataCode": code,
+        }
+        response = requests.put(url=self.airport_sheet, json=body)
--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements
+import data_manager
+import flight_search
+
+sheet = data_manager.DataManager(url="https://api.sheety.co/3871b3bd1969f6ec4c9cd8a5b99e7397/flightDeals/prices")
+
+sheet_data = sheet.get_sheet()
+
+
+for data in sheet_data:
+    if data['iataCode'] == '':
+        iataCode = flight_search.FlightSearch.search_code(city=data['city'])
+        data_manager.DataManager.set_iata_code(self=data, city=data['city'], code=iataCode)
+
+print(sheet_data)

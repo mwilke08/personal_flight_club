ae9a1a2f97236ccbfe4136d70286f8ffedd849a1
Matt Wilke <matt.wilke08@gmail.com>
2021-08-11 21:19:56 -0500

    couple flight changes

--- data_manager.py
+++ data_manager.py
@@ -1,19 +1,25 @@
 import requests
 from pprint import pprint
 
+FLIGHT_URL = "https://api.sheety.co/3871b3bd1969f6ec4c9cd8a5b99e7397/flightDeals/prices"
+
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    def __init__(self, url):
-        self.airport_sheet = url
+    # This class is responsible for talking to the Google Sheet.
+    def __init__(self):
+        self.destinations = {}
 
     def get_sheet(self):
-        response = requests.get(url=self.airport_sheet)
+        response = requests.get(url=FLIGHT_URL)
         data = response.json()
+        self.destinations = data['prices']
+
         return data['prices']
 
-    def set_iata_code(self, city, code):
-        body = {
-            "city": city,
-            "iataCode": code,
+    def set_code(self, city, code):
+        new_data = {
+            "price": {
+                "iataCode": city[code]
+            }
         }
-        response = requests.put(url=self.airport_sheet, json=body)
+        response = requests.put(url=f"{FLIGHT_URL}/{city['id']}", json=new_data)
--- main.py
+++ main.py
@@ -2,14 +2,13 @@
 import data_manager
 import flight_search
 
-sheet = data_manager.DataManager(url="https://api.sheety.co/3871b3bd1969f6ec4c9cd8a5b99e7397/flightDeals/prices")
+sheet = data_manager.DataManager()
 
 sheet_data = sheet.get_sheet()
 
-
 for data in sheet_data:
     if data['iataCode'] == '':
-        iataCode = flight_search.FlightSearch.search_code(city=data['city'])
-        data_manager.DataManager.set_iata_code(self=data, city=data['city'], code=iataCode)
+        new_code = flight_search.FlightSearch.search_code(city=data['city'])
+        data_manager.DataManager.set_code(city=data['city'], code=new_code)
 
 print(sheet_data)
